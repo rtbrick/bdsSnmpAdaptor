e187cf64c5516429b8096cb06761be07aa2c37ed
Ilya Etingof <etingof@gmail.com>
2019-05-19 09:19:08 +0200

    Migrate system software info onto MIB-based population

--- bdssnmpadaptor/mapping_modules/confd_local_system_software_info_confd.py
+++ bdssnmpadaptor/mapping_modules/confd_local_system_software_info_confd.py
@@ -5,10 +5,8 @@
 # Copyright (C) 2017-2019, RtBrick Inc
 # License: BSD License 2.0
 #
-from pysnmp.proto.rfc1902 import OctetString
 
 from bdssnmpadaptor.mapping_functions import BdsMappingFunctions
-from bdssnmpadaptor.oidDb import OidDbItem
 
 
 class ConfdLocalSystemSoftwareInfoConfd(object):
@@ -18,54 +16,46 @@ class ConfdLocalSystemSoftwareInfoConfd(object):
     @classmethod
     async def setOids(cls, bdsJsonResponseDict, targetOidDb,
                       tableSequenceList, birthday):
+
         swString = BdsMappingFunctions.stringFromSoftwareInfo(bdsJsonResponseDict)
 
-        targetOidDb.insertOid(
-            newOidItem=OidDbItem(
-                bdsMappingFunc=__name__,
-                oid='1.3.6.1.2.1.1.1.0',
-                name='sysDescr', pysnmpBaseType=OctetString,
-                value=swString))
+        targetOidDb.setLock()
 
-        # oidSegment = "1.3.6.1.2.1.1.1.0"
-        # targetOidDb.setLock()
-        # targetOidDb.deleteOidsWithPrefix(oidSegment)  #delete existing TableOids
-        # for index,bdsJsonObject in enumerate(bdsJsonResponseDict['objects"]):
-        #     #indexString = bdsJsonObject["attribute"]["library"]
-        #     #indexCharList = [str(ord(c)) for c in indexString]
-        #     #index = str(len(indexCharList)) + "." + ".".join(indexCharList)  # FIXME add description
-        #     targetOidDb.insertOid(newOidItem = OidDbItem(
-        #         oid = oidSegment+"1."+str(index),
-        #         name="hrSWRunIndex", pysnmpBaseType=Integer32,
-        #         value=index))
-        #     targetOidDb.insertOid(newOidItem = OidDbItem(
-        #         oid = oidSegment+"2."+str(index),
-        #         name="hrSWRunName", pysnmpBaseType=OctetString,
-        #         value=bdsJsonObject["attribute"]["commit_id"]))
-        #     targetOidDb.insertOid(newOidItem = OidDbItem(
-        #         oid = oidSegment+"3."+str(index),
-        #         name="commitDate", pysnmpBaseType=OctetString,
-        #         value=bdsJsonObject["attribute"]["commit_date"]))
-        #     targetOidDb.insertOid(newOidItem = OidDbItem(
-        #         oid = oidSegment+"4."+str(index),
-        #         name="packageDate", pysnmpBaseType=OctetString,
-        #         value=bdsJsonObject["attribute"]["package_date"]))
-        #     targetOidDb.insertOid(newOidItem = OidDbItem(
-        #         oid = oidSegment+"5."+str(index),
-        #         name="vcCheckout", pysnmpBaseType=OctetString,
-        #         value=bdsJsonObject["attribute"]["vc_checkout"]))
-        #     targetOidDb.insertOid(newOidItem = OidDbItem(
-        #         oid = oidSegment+"6."+str(index),
-        #         name="branch", pysnmpBaseType=OctetString,
-        #         value=bdsJsonObject["attribute"]["branch"]))
-        #     targetOidDb.insertOid(newOidItem = OidDbItem(
-        #         oid = oidSegment+"7."+str(index),
-        #         name="libraryVersion", pysnmpBaseType=OctetString,
-        #         value=bdsJsonObject["attribute"]["version"]))
-        #     targetOidDb.insertOid(newOidItem = OidDbItem(
-        #         oid = oidSegment+"8."+str(index),
-        #         name="sourcePath", pysnmpBaseType=OctetString,
-        #         value=bdsJsonObject["attribute"]["source_path"]))
-        # # in addition the SW Info Flag is set by
-        # # creating an abbreviated string over all modules
-        # targetOidDb.releaseLock()
+        with targetOidDb.module(__name__) as add:
+
+            add('IF-MIB', 'ifDescr', 1, value=swString)
+
+            # targetOidDb.deleteOidsWithPrefix(oidSegment)  #delete existing TableOids
+
+            # for index,bdsJsonObject in enumerate(bdsJsonResponseDict['objects"]):
+            #     #indexString = bdsJsonObject["attribute"]["library"]
+            #     #indexCharList = [str(ord(c)) for c in indexString]
+            #     #index = str(len(indexCharList)) + "." + ".".join(indexCharList)  # FIXME add description
+            #
+            #     add('HOST-RESOURCES-MIB', 'hrSWRunIndex', index, value=index)
+            #
+            #     add('HOST-RESOURCES-MIB', 'hrSWRunName', index,
+            #          value=bdsJsonObject["attribute"]["commit_id"])
+
+            #     add('HOST-RESOURCES-MIB', 'commitDate', index,
+            #         value=bdsJsonObject["attribute"]["commit_date"])
+
+            #     add('HOST-RESOURCES-MIB', 'packageDate', index,
+            #         value=bdsJsonObject["attribute"]["package_date"])
+
+            #     add('HOST-RESOURCES-MIB', 'vcCheckout', index,
+            #         value=bdsJsonObject["attribute"]["vc_checkout"])
+
+            #     add('HOST-RESOURCES-MIB', 'branch', index,
+            #         value=bdsJsonObject["attribute"]["branch"])
+
+            #     add('HOST-RESOURCES-MIB', 'libraryVersion', index,
+            #         value=bdsJsonObject["attribute"]["version"])
+
+            #     add('HOST-RESOURCES-MIB', 'sourcePath', index,
+            #         value=bdsJsonObject["attribute"]["source_path"])
+
+            # # in addition the SW Info Flag is set by
+            # # creating an abbreviated string over all modules
+
+        targetOidDb.releaseLock()
